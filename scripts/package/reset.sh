d42f7ba4939c2eb8c61c38a08902c4ab08160b4e
mzhvv <maximmezhevov@gmail.com>
2026-08-12 19:41:38 +0300

    .

--- scripts/package/reset.sh
+++ scripts/package/reset.sh
@@ -1,4 +1,8 @@
 #!/bin/bash # scripts/package/reset.sh
 
-bash scripts/package/removing.sh
-bash scripts/package/installing.sh
+echo ""
+echo "will run scripts/package/removing.sh && scripts/package/installing.sh"
+echo ""
+
+bash scripts/package/removing.sh && bash scripts/package/installing.sh
+
