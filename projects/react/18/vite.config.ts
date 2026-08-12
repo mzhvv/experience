fce331f2b5ae9be705c4507a9ccd318a36d205b7
mzhvv <maximmezhevov@gmail.com>
2026-08-13 00:06:19 +0300

    .

--- projects/react/18/vite.config.ts
+++ projects/react/18/vite.config.ts
@@ -3,20 +3,21 @@
 import path from 'path';
 import { fileURLToPath } from 'url';
 
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+import type { AliasOptions } from 'vite';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
-
-const aliases = {
+const alias = {
   '@': path.resolve(__dirname, './src'),
   '@packages': path.resolve(__dirname, './packages'),
-};
+} satisfies AliasOptions;
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
-    alias: aliases,
+    alias,
   },
 });
