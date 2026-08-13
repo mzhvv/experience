3b245290eeedd03d5ddd56db404ef78dca4efefa
mzhvv <maximmezhevov@gmail.com>
2026-08-13 16:50:30 +0300

    .

--- projects/react/18/src/app/main.tsx
+++ projects/react/18/src/app/main.tsx
@@ -2,8 +2,12 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 
+import { Button } from '@packages/ui/components/ui/button';
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <div>a</div>
+    <div>
+      <Button>a</Button>
+    </div>
   </StrictMode>
 );
