cddcf6e08a2e9a35404879b59f3c7748ac1399e9
mzhvv <maximmezhevov@gmail.com>
2026-09-21 07:23:08 +0300

    .

--- packages/libs/vite.config.ts
+++ packages/libs/vite.config.ts
@@ -18,7 +18,7 @@ export default defineConfig({
       staticImport: true,
       clearPureImport: true,
 
-      exclude: ['_config/**', '_bin/**'],
+      exclude: ['_config/**', '_bin/**', '**/*.test.ts'],
     }),
   ],
 
@@ -68,24 +68,3 @@ export default defineConfig({
     },
   },
 });
-
-/* 
-  ✅ 20.09.26
-
-  vite v6.4.3 building for production...
-  ✓ 9 modules transformed.
-
-  [vite:dts] Start generate declaration files...
-  dist/npm/core/index.js                       0.12 kB │ gzip: 0.11 kB
-  dist/npm/bin/add-scripts.js                  0.15 kB │ gzip: 0.13 kB
-  dist/npm/bin/publish-version-with-token.js   0.23 kB │ gzip: 0.16 kB
-  dist/npm/libs/get-npm-token.js               0.33 kB │ gzip: 0.23 kB
-  dist/npm/libs/index.js                       0.45 kB │ gzip: 0.23 kB
-  dist/npm/libs/update-package-version.js      0.53 kB │ gzip: 0.33 kB
-  dist/npm/libs/npm.js                         0.55 kB │ gzip: 0.29 kB
-  dist/npm/core/publish-version-with-token.js  0.81 kB │ gzip: 0.44 kB
-  dist/npm/libs/add-scripts.js                 0.95 kB │ gzip: 0.49 kB
-  [vite:dts] Declaration files built in 2950ms.
-
-  ✓ built in 3.19s
-*/
